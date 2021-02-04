cd0b80103bdef2873555cccef57647304b7a938e
FinnHannes <78381325+FinnHannes@users.noreply.github.com>
2021-02-04 21:55:54 +0100

    Define basic task object

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const taskMessage = {
+    verb: {},
+    article: {},
+    adjective: {},
+    helpNoun: {},
+    preposition: {},
+    theLocationNoun: {},
+    createRandomTask () {
+        
+    }
+}
