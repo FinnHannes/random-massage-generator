6ca34a50481134ee22ea103c008d4d4524320615
FinnHannes <78381325+FinnHannes@users.noreply.github.com>
2021-02-04 23:33:31 +0100

    Add final function random message and more examples

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Object with a property for every sencence part and a method to create a random sentence
 const taskMessage = {
-    verb: ['rescue', 'help'],
+    verb: ['rescue', 'help', 'safe', 'attack'],
     article: ['the', 'a'],
-    adjective: ['small', 'black'],
+    adjective: ['small', 'tiny', 'big', ],
     // noun whom to help
-    helpNoun: ['cat', 'lady'],
-    preposition: ['from', 'on'],
+    helpNoun: ['cat', 'lady', 'grandpa', 'child', 'worm'],
+    preposition: ['from', 'on', 'off', 'under'],
     // noun with the location where to help plus the corresponding article
-    theLocationNoun: ['a tree', 'the street'],
+    theLocationNoun: ['a tree', 'the street', 'the earth', 'the underground'],
     // creates the random generated task with a word from every property
     createRandomTask () {
         // array for the random generated task
@@ -19,10 +19,10 @@ const taskMessage = {
         // returns a string with the values from the array, each concatenated with a space character
         return randomtask.join(' ');
     }
-}
+};
 
 // generates a random number between 1 and 100
-const getCharmaPoints = () => {
+function getCharmaPoints() {
     const charmaPoints = Math.ceil(Math.random() * 100);
     return charmaPoints;
 }
@@ -30,6 +30,11 @@ const getCharmaPoints = () => {
 // Array with the recommendations for the charm points 
 const recommendationsUsePoints = ['use a bad word', " don't say thank you", "don't greet the neighbors"];
 
+// creates the three final sentences with the randomly created content
+function createRandomMessage() {
+    console.log(`Your task for today is: ${taskMessage.createRandomTask()}.`);
+    console.log(`For this you will get ${getCharmaPoints()} charma points.`);
+    console.log(`You could use them the next time you ${recommendationsUsePoints[Math.floor(Math.random() * recommendationsUsePoints.length)]}.`);
+}
 
-
-console.log(getCharmaPoints());
+console.log(createRandomMessage());
